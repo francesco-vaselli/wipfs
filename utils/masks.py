003bbbb6cbaf9faac9bbe252d841b4a6f8604192
francesco-vaselli <francesco.vaselli@protonmail.com>
2022-12-08 16:46:20 +0100

    utilities

---
+++ utils/masks.py
@@ -0,0 +1,20 @@
+from nflows.utils import typechecks as check
+import numpy as np
+import torch
+
+
+def create_block_binary_mask(features, block_size):
+    """
+    Creates a binary mask of a given dimension which splits its masking into blocks of a given size.
+    :param features: Dimension of mask.
+    :param block_size: Size of blocks to split mask into.
+    :return: Binary mask split into blocks of type torch.Tensor
+    """
+    if not check.is_positive_int(features):
+        raise ValueError("Number of features must be a positive integer.")
+    if not check.is_positive_int(block_size):
+        raise ValueError("Block size must be a positive integer.")
+
+    mask = torch.zeros(features).byte()
+    mask[:block_size] += 1
+    return mask
---
+++ utils/permutations.py
@@ -0,0 +1,22 @@
+import torch
+from nflows.transforms.permutations import Permutation
+
+
+class BlockPermutation(Permutation):
+    """Permutes just a block of lenght `block_size` at a time.
+    NOTE: we are moving a number of elements equal to `block_size` to the right."""
+
+    def __init__(self, features, block_size, dim=1):
+        if not check.is_positive_int(features):
+            raise ValueError("Number of features must be a positive integer.")
+        if not check.is_positive_int(block_size):
+            raise ValueError("Block size must be a positive integer.")
+
+        super().__init__(
+            torch.hstack(
+                [
+                    torch.arange(features)[block_size:],
+                    torch.arange(features)[:block_size],
+                ]
+            )
+        )

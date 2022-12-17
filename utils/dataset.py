94cd976a2d20553078c6c3f23b4d15ad9255d736
fcattafesta <filippo.cattafesta99@gmail.com>
2022-12-17 15:19:00 +0100

    Electrons basic nflow model

--- utils/dataset.py
+++ utils/dataset.py
@@ -32,3 +32,37 @@ class MyDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.x_train[idx], self.y_train[idx]
+
+
+class ReadDataset(Dataset):
+    """Very simple Dataset for reading hdf5 data
+        This is way simpler than muons as we heve enough jets in a single file
+        Still, dataloading is a bottleneck even here
+    Args:
+        Dataset (Pytorch Dataset): Pytorch Dataset class
+    """
+
+    def __init__(self, h5_paths, limit, x_maxidx, y_maxidx):
+        
+        # we must fix a convention for parametrize slices
+
+        self.h5_paths = h5_paths
+        self._archives = [h5py.File(h5_path, "r") for h5_path in self.h5_paths]
+        self._archives = None
+
+        y = self.archives[0]["data"][:limit, 0:y_maxidx]
+        x = self.archives[0]["data"][:limit, y_maxidx:x_maxidx]
+        self.x_train = torch.tensor(x, dtype=torch.float32)  # .to(device)
+        self.y_train = torch.tensor(y, dtype=torch.float32)  # .to(device)
+
+    @property
+    def archives(self):
+        if self._archives is None:  # lazy loading here!
+            self._archives = [h5py.File(h5_path, "r") for h5_path in self.h5_paths]
+        return self._archives
+
+    def __len__(self):
+        return len(self.y_train)
+
+    def __getitem__(self, idx):
+        return self.x_train[idx], self.y_train[idx]

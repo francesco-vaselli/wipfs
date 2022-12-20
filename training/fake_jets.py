45339b56dad3a5ffa12bd9a78bccf85e544c87b3
francesco-vaselli <francesco.vaselli@protonmail.com>
2022-12-20 12:01:16 +0100

    added correct printout

--- training/fake_jets.py
+++ training/fake_jets.py
@@ -78,4 +78,4 @@ if __name__ == "__main__":
     train_loader = DataLoader(
             train_ds, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, num_workers=9
         )
-    print(train_loader.next())
+    print(next(iter(train_loader)))

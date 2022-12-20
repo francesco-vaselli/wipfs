496bff177b0334c1e1271720a55e1c1bd62177ce
francesco-vaselli <francesco.vaselli@protonmail.com>
2022-12-20 11:40:27 +0100

    indent

--- training/fake_jets.py
+++ training/fake_jets.py
@@ -15,65 +15,65 @@ BATCH_SIZE = 2048
 
 
 if __name__ == "__main__":
-# the args dictionary defining all the parameters for the FakeDoubleFlow model
-args = {
-    'zdim': 128,
-    'input_dim': 3,
-    'optimizer': 'adam'
-    'lr': 0.001,
-    'weight_decay': 0.0,
-    'beta1': 0.9,
-    'beta2': 0.999,
-    'entropy_weight': 1.0,
-    'prior_weight': 1.0,
-    'recon_weight': 1.0,
-    'use_deterministic_encoder': False,
-    'use_latent_flow': True,
-    'latent_flow_param_dict': {
-        "input_dim" : 17,
-        "context_dim" : 14,
-        "num_flow_steps" : 15,
+    # the args dictionary defining all the parameters for the FakeDoubleFlow model
+    args = {
+        'zdim': 128,
+        'input_dim': 3,
+        'optimizer': 'adam'
+        'lr': 0.001,
+        'weight_decay': 0.0,
+        'beta1': 0.9,
+        'beta2': 0.999,
+        'entropy_weight': 1.0,
+        'prior_weight': 1.0,
+        'recon_weight': 1.0,
+        'use_deterministic_encoder': False,
+        'use_latent_flow': True,
+        'latent_flow_param_dict': {
+            "input_dim" : 17,
+            "context_dim" : 14,
+            "num_flow_steps" : 15,
 
-        "base_param_dict" : {
-        "num_transform_blocks": 10,
-        "activation": "relu",
-        "batch_norm": True,
-        "num_bins": 128,
-        "hidden_dim": 298,
-        "block_size": 3,
-	    "mask_type" : "identity"
+            "base_param_dict" : {
+            "num_transform_blocks": 10,
+            "activation": "relu",
+            "batch_norm": True,
+            "num_bins": 128,
+            "hidden_dim": 298,
+            "block_size": 3,
+            "mask_type" : "identity"
+            },
+
+            "transform_type" : "no-permutation" 
         },
+        'reco_flow_param_dict': {
+                    "input_dim" : 17,
+            "context_dim" : 14,
+            "num_flow_steps" : 15,
 
-        "transform_type" : "no-permutation" 
-    },
-    'reco_flow_param_dict': {
-                "input_dim" : 17,
-        "context_dim" : 14,
-        "num_flow_steps" : 15,
+            "base_param_dict" : {
+            "num_transform_blocks": 10,
+            "activation": "relu",
+            "batch_norm": True,
+            "num_bins": 128,
+            "hidden_dim": 298,
+            "block_size": 3,
+            "mask_type" : "identity"
+            },
 
-        "base_param_dict" : {
-        "num_transform_blocks": 10,
-        "activation": "relu",
-        "batch_norm": True,
-        "num_bins": 128,
-        "hidden_dim": 298,
-        "block_size": 3,
-	    "mask_type" : "identity"
+            "transform_type" : "no-permutation" 
         },
+    }
 
-        "transform_type" : "no-permutation" 
-    },
-}
+    # create the model
+    model = FakeDoubleFlow(args)
 
-# create the model
-model = FakeDoubleFlow(args)
+    # print total params number and stuff
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-# print total params number and stuff
-total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-# define dataset
-train_ds = FakeDataset(["./datasets/fake_jets.hdf5"], limit=1000000)
-train_loader = DataLoader(
-        train_ds, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, num_workers=9
-    )
-print(train_loader.next())
+    # define dataset
+    train_ds = FakeDataset(["./datasets/fake_jets.hdf5"], limit=1000000)
+    train_loader = DataLoader(
+            train_ds, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, num_workers=9
+        )
+    print(train_loader.next())

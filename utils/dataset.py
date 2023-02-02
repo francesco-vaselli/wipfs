0719e050deb9eff09e1ac64cbc85aea06d4a1fb2
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-02 14:20:45 +0100

    z rescaled

--- utils/dataset.py
+++ utils/dataset.py
@@ -125,7 +125,8 @@ class SimpleFakesDataset(Dataset):
         x = self.archives[0]["data"][start:limit, 0:x_dim]
         z = self.archives[0]["data"][start:limit, (6 + x_dim) : (6 + x_dim + z_dim)]
         self.x_train = torch.tensor(x, dtype=torch.float32) # .view(-1, 1, x_dim) no reshape because no conv1d
-        self.y_train = torch.tensor(y, dtype=torch.float32)  
+        self.y_train = torch.tensor(y, dtype=torch.float32) 
+        z[:, 1] = z[:, 1] / 200.0
         self.z_train = torch.tensor(z, dtype=torch.float32) 
 
     @property

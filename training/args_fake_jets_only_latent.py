af30cdd59acddee1ccd0d8e03fef05f17ea7431d
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-03 12:07:04 +0100

    new 4 var training2

--- training/args_fake_jets_only_latent.py
+++ training/args_fake_jets_only_latent.py
@@ -24,7 +24,7 @@ def add_args(parser):
             "batch_norm": True,
             "num_bins": 32,
             "hidden_dim": 128,
-            "base_transform_type": "rq-autoregressive",
+            "base_transform_type": "rq-coupling" # "rq-autoregressive",
             # "block_size": 2, # useless param if we have alternating-binary mask
             # "mask_type" : "alternating-binary"
             },

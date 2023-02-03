e9eea736a24a7ce0d7aa5ce4a9fee1473c88d122
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-03 17:04:50 +0100

    validation ranges fixed

--- utils/fake_utils.py
+++ utils/fake_utils.py
@@ -1179,11 +1179,11 @@ def validate_latent_flow(
             #     np.arange(left_of_first_bin, right_of_last_bin + d, d),
             #     np.arange(left_of_first_bin2, right_of_last_bin2 + d2, d2),
             # ],
-            range=[[0, 100], [0, 11]],
+            range=[[0, 100], [0, 1.1]],
             cmap="Reds",
             label="FlashSim Latent",
         )
-        ax2.set_ylim([0, 11])
+        ax2.set_ylim([0, 1.1])
         ax2.set_xlabel("PU_n_true_int")
         ax2.set_ylabel("N_true_fakes_latent")
         

acfe0482ec8c2b4104ab50e6884d2761bc7fb332
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-14 17:31:59 +0100

    rejets validation finished (hopefully)

--- training/validate_rejets.py
+++ training/validate_rejets.py
@@ -138,3 +138,119 @@ def validate_rejets(
     plt.suptitle('Jet tagging distributions correlations', fontsize=20)
     writer.add_figure("Jet tagging correlations", fig, global_step=epoch)
     plt.close()
+
+    blue_line = mlines.Line2D([], [], color='tab:blue', label='FullSim')
+    red_line = mlines.Line2D([], [], color='tab:orange', label='FlashSim')
+    fig = corner.corner(reco[:, [13, 10]], bins=40, labels=['Jet_qgl', 'Jet_nConstituents'], color='tab:blue', smooth1d=0.5,
+                        levels=(0.5,0.9, 0.99), hist_bin_factor=1, scale_hist=True, plot_datapoints=False)
+    corner.corner(samples[:, [13, 10]], bins=40, levels=[0.5, 0.9, 0.99], hist_bin_factor=1, color='tab:orange', smooth1d=0.5,
+                scale_hist=True, plot_datapoints=False, fig=fig)
+    plt.legend(fontsize=16, frameon=False, handles=[blue_line,red_line], bbox_to_anchor=(0., 1.0, 1., 1.0), loc='upper right')
+    plt.suptitle('qgl and nConstituens correlations', fontsize=16)
+    writer.add_figure('qgl and nConstituens correlations', fig, global_step=epoch)
+    plt.close()
+
+    blue_line = mlines.Line2D([], [], color='tab:blue', label='FullSim')
+    red_line = mlines.Line2D([], [], color='tab:orange', label='FlashSim')
+    fig = corner.corner(reco[:, [12, 9]], range=[(0, 100), (0,40)], labels=['Jet_pt [GeV]', 'Jet_mass [GeV]'], color='tab:blue',
+                        levels=(0.5,0.9, 0.99), hist_bin_factor=3, scale_hist=True, plot_datapoints=False)
+    corner.corner(samples[:, [12, 9]], range=[(0, 100), (0,40)], levels=[0.5, 0.9, 0.99], hist_bin_factor=3, color='tab:orange',
+                scale_hist=True, plot_datapoints=False, fig=fig)
+    plt.legend(fontsize=16, frameon=False, handles=[blue_line,red_line], bbox_to_anchor=(0., 1.0, 1., 1.0), loc='upper right')
+    plt.suptitle(r'p$_T$ and mass correlations', fontsize=16
+                )
+    writer.add_figure(r'p$_T$ and mass correlations', fig, global_step=epoch)
+    plt.close()
+
+    limited_pt = total[:, 12]
+    limited_ptj = totalj[:, 12]
+    gen = df.loc[:, 'GenJet_pt'].values
+    limited = np.vstack([gen[:len(limited_pt)], limited_pt]).T
+    limitedj = np.vstack([gen[:len(limited_ptj)], limited_ptj]).T
+
+    blue_line = mlines.Line2D([], [], color='tab:blue', label='FullSim')
+    red_line = mlines.Line2D([], [], color='tab:orange', label='FlashSim')
+    fig = corner.corner(limited, range=[(0,100), (0,100)], labels=['GenJet_pt [GeV]', 'Jet_pt [GeV]'], color='tab:blue',
+                        levels=(0.5,0.9, 0.99), hist_bin_factor=3, scale_hist=True, plot_datapoints=False)
+    corner.corner(limitedj, range=[(0,100), (0,100)], levels=[0.5, 0.9, 0.99], hist_bin_factor=3, color='tab:orange',
+                scale_hist=True, plot_datapoints=False, fig=fig)
+    plt.legend(fontsize=16, frameon=False, handles=[blue_line,red_line], bbox_to_anchor=(0., 1.0, 1., 1.0), loc='upper right')
+    plt.suptitle(r'GenJet_p$_T$ and p$_T$ correlations', fontsize=16
+                )
+    writer.add_figure(r'GenJet_p$_T$ and p$_T$ correlations', fig, global_step=epoch)
+    plt.close()    
+
+    # b-tagging FOM
+
+    def histANDroc(gen, gen_df):
+        truth = np.abs(gen_df.values)
+        mask_b = np.where(truth[:, 12]==5)
+        mask_uds = np.where((truth[:, 12]==1) | (truth[:, 12]==2) | (truth[:, 12]==3))
+        print(truth[:, 12])
+        bs = gen[mask_b, 2].flatten()
+        nbs = gen[mask_uds, 2].flatten()
+        # nbs = nbs[0:len(bs)]
+
+        bs = bs[bs >=-0.05]
+        nbs = nbs[nbs >=-0.05]
+
+        bs = np.where(bs<0, 0, bs)
+        nbs = np.where(nbs<0, 0, nbs)
+
+        bs = np.where(bs>1, 1, bs)
+        nbs = np.where(nbs>1, 1, nbs)
+
+        # bs = bs[0:len(nbs)]
+
+        figure = plt.figure(figsize=(9, 6.5))
+        ax = plt.gca()
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        plt.hist(bs.flatten(), bins=50, label="b",  histtype='step', lw=2, color='C2')
+        plt.hist(nbs.flatten(), bins=50, label="uds",  histtype='step', lw=2, color='C3')
+        plt.title("FlashSim Jet_btagDeepB for b ground truth", fontsize=16)
+        plt.legend(fontsize=16, frameon=False, loc='upper left')
+
+        y_bs = np.ones(len(bs))
+        y_nbs = np.zeros(len(nbs))
+        y_t = np.concatenate((y_bs, y_nbs))
+        y_s = np.concatenate((bs, nbs))
+
+        fpr, tpr, _ = roc_curve(y_t.ravel(), y_s.ravel())
+        roc_auc = auc(fpr, tpr)
+
+        return figure, fpr, tpr, roc_auc, bs, nbs
+
+    fig, fpr, tpr, roc_auc, bs, nbs = histANDroc(samples, gen)
+    cfig, cfpr, ctpr, croc_auc, cbs, cnbs  = histANDroc(reco, gen)
+
+    fig = plt.figure(figsize=(9, 6.5))
+    lw = 2
+    plt.plot(
+        tpr,
+        fpr,
+        color="C1",
+        lw=lw,
+        label=f"ROC curve (area = %0.2f) FlashSim" % roc_auc,
+    )
+    plt.plot(
+        ctpr,
+        cfpr,
+        color="C0",
+        lw=lw,
+        label="ROC curve (area = %0.2f) FullSim" % croc_auc,
+    )
+
+    #plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+    #plt.xlim([0.0, 1.0])
+    plt.yscale("log")
+    plt.ylim([0.0005, 1.05])
+    plt.xlabel("Efficency for b-jet (TP)", fontsize=16)
+    plt.ylabel("Mistagging prob (FP)", fontsize=16)
+    ax = plt.gca()
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    plt.title("Receiver operating characteristic", fontsize=16)
+    plt.legend(fontsize=16, frameon=False,loc="best")
+    writer.add_figure("ROC", fig, global_step=epoch)
+    plt.close()

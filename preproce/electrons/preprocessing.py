ea4965aa14e5c0eb6575405ae4851d7923bc4d5e
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-17 17:17:32 +0100

    Uniform smearing and [-1, 1] map added

--- preproce/electrons/preprocessing.py
+++ preproce/electrons/preprocessing.py
@@ -1,4 +1,4 @@
-import sys
+import  os
 import json
 import h5py
 import pandas as pd
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")  # temporary for MatPlotLibDeprecationWarning 
 
 from prep_actions import vars_dictionary  # operation dictionary
 
-np.random.seed(0)  # fixed seed for gaussian random smearing
+np.random.seed(0)  # fixed seed for random smearing
 
 # NOTE: uproot 5 update has changed the way to access the TTree
 # as a multiindex pandas dataframe, in case of future errors the sintax should be:
@@ -62,7 +62,7 @@ def saturation(df, column_name, interval):
 
 def gaus_smearing(df, column_name, sigma, interval):
     """
-    Performs gaussian smearing on given column. If interval is specified, random gaussian data are asseigned to column in interval.
+    Performs gaussian smearing on given column. If interval is specified, random gaussian data are assigned to column in interval.
     """
     val = df[column_name].values
     if interval != None:
@@ -81,6 +81,26 @@ def gaus_smearing(df, column_name, sigma, interval):
         )
     return df[column_name]
 
+def unif_smearing(df, column_name, half_width, interval):
+    """
+    Performs uniform smearing on given column. If interval is specified, random uniform data are assigned to column in interval.
+    """
+    val = df[column_name].values
+    if interval != None:
+        mask_condition = np.logical_and(val >= interval[0], val <= interval[1])
+        loc = np.mean(val[mask_condition])
+        print(
+            f"Creating uniform data (loc={loc}, half_width={half_width}) in range {interval}..."
+        )
+        val[mask_condition] = np.random.uniform(
+            low=loc-half_width, high=loc+half_width, size=val[mask_condition].shape
+        )
+    else:
+        print(f"Unifrom smearing with half_width={half_width}...")
+        df[column_name] = df[column_name].apply(
+            lambda x: x + half_width * np.random.uniform(-1, 1)
+        )
+    return df[column_name]
 
 def transform(df, column_name, function, p):
     """
@@ -89,6 +109,15 @@ def transform(df, column_name, function, p):
     print(f"Applying {function} with parameters {p}...")
     df[column_name] = df[column_name].apply(lambda x: function(x * p[0] + p[1]))
     return df[column_name]
+
+def fix_range(column_name, df):
+
+    scale_factor = np.max(np.abs(df[column_name].values))
+
+    df[column_name] = df[column_name] / scale_factor
+
+    return df[column_name], scale_factor
+
 
 
 def process_column_var(column_name, operations, df):
@@ -106,6 +135,11 @@ def process_column_var(column_name, operations, df):
             mask_condition = op[2]
             df[column_name] = gaus_smearing(df, column_name, sigma, mask_condition)
 
+        if op[0] == "u":
+            half_width = op[1]
+            mask_condition = op[2]
+            df[column_name] = unif_smearing(df, column_name, half_width, mask_condition)
+
         if op[0] == "t":
             function = op[1]
             p = op[2]
@@ -121,18 +155,23 @@ def preprocessing(df, vars_dictionary):
     """
     Preprocessing general function given any dataframe and its dictionary
     """
-    df = df[~df.isin([np.nan, np.inf, -np.inf]).any(axis="columns")]
-
+    dict_to_save = {}
     for column_name, operation in vars_dictionary.items():
         fig, axs = plt.subplots(1, 2)
         plt.suptitle(f"{column_name}")
         axs[0].hist(df[column_name], bins=30, histtype="step")
         df[column_name] = process_column_var(column_name, operation, df)
+        df[column_name], scale = fix_range(column_name, df)
+        dict_to_save[column_name] = scale
         axs[1].hist(df[column_name], bins=30, histtype="step")
         plt.savefig(f"figures/{column_name}.pdf", format="pdf")
         plt.close()  # produces MatplotlibDeprecationWarning. It is a bug (https://github.com/matplotlib/matplotlib/issues/23921)
 
-    df = df[~df.isin([np.nan, np.inf, -np.inf]).any(axis="columns")]
+    f = open("scale_factors.json", "w")
+    f.write(json.dumps(dict_to_save))
+    f.close()
+
+    os.system("cp scale_factors.json ../../training/electrons/")  
 
     return df
 
@@ -150,6 +189,8 @@ if __name__ == "__main__":
         df.reset_index(drop=True)
 
     df = preprocessing(df, vars_dictionary)
+    df = df[~df.isin([np.nan, np.inf, -np.inf]).any(axis="columns")]
+
     print(df.columns)
     file = h5py.File(f"MElectrons.hdf5", "w")
 

39d296344b7d074d0f6c148aec8fd7ebd855210f
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-18 15:18:51 +0100

    KS test added

--- training/electrons/processing_test.py
+++ training/electrons/processing_test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import h5py
 
-from scipy.stats import wasserstein_distance
+from scipy.stats import wasserstein_distance, ks_2samp
 
 from postprocessing import gen_columns, reco_columns
 
@@ -16,12 +16,20 @@ df_post = pd.DataFrame(data=f.get("data"), columns=gen_columns + reco_columns)
 f.close()
 
 ws_dict = {}
+ks_dict = {}
 
 for column_name in reco_columns:
     ws_dict[column_name] = wasserstein_distance(
         df_pre[column_name].values, df_post[column_name].values
     )
+    ks_pvalue = ks_2samp(df_pre[column_name].values, df_post[column_name].values)[1]
+    ks_dict[column_name] = ks_pvalue
 
 f = open("ws.json", "w")
 f.write(json.dumps(ws_dict))
 f.close()
+
+f = open("ks.json", "w")
+f.write(json.dumps(ks_dict))
+f.close()
+

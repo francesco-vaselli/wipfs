845c37c9ebcf17ac610ff34b58f1d1402392d4dc
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-21 11:37:26 +0100

    Validation function: 1D distributions and corner plots

--- training/electrons/corner_plots.py
+++ training/electrons/corner_plots.py
@@ -2,114 +2,154 @@ import numpy as np
 import pandas as pd
 import h5py
 
-from corner import corner
+import corner
 from matplotlib import pyplot as plt
 from matplotlib import lines as mlines
 
 from postprocessing import reco_columns, gen_columns
 
-f = h5py.File("MElectrons_post.hdf5", "r")
-df = pd.DataFrame(data=f.get("data"), columns=gen_columns + reco_columns)
-f.close()
 
-# Corner plot: kinematics
+def make_corner(reco, samples, labels, title, *args, **kwargs):
+    blue_line = mlines.Line2D([], [], color="tab:blue", label="FullSim")
+    red_line = mlines.Line2D([], [], color="tab:orange", label="FlashSim")
+    fig = corner.corner(
+        reco[labels],
+        labels=labels,
+        color="tab:blue",
+        levels=[0.5, 0.9, 0.99],
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+        *args,
+        **kwargs
+    )
+    corner.corner(
+        samples[labels],
+        fig=fig,
+        color="tab:orange",
+        levels=[0.5, 0.9, 0.99],
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+        *args,
+        **kwargs
+    )
+    plt.legend(
+        fontsize=24,
+        frameon=False,
+        handles=[blue_line, red_line],
+        bbox_to_anchor=(0.0, 1.0, 1.0, 4.0),
+        loc="upper right",
+    )
+    plt.suptitle(title, fontsize=20)
+    plt.close()
+    return fig
 
-df["MElectron_pt"] = df["MElectron_ptRatio"] * df["MGenElectron_pt"]
-df["MElectron_eta"] = df["MElectron_etaMinusGen"] + df["MGenElectron_eta"]
-df["MElectron_phi"] = df["MElectron_phiMinusGen"] + df["MGenElectron_phi"]
 
-labels = ["MElectron_pt", "MElectron_eta", "MElectron_phi"]
+if __name__ == "__main__":
 
-fig = corner(
-    df[labels],
-    labels=labels,
-    color="tab:blue",
-    levels=(0.5, 0.9, 0.99),
-    hist_bin_factor=3,
-    scale_hist=True,
-    plot_datapoints=False,
-)
-plt.suptitle("MElectrons kinematics variables", fontsize=20)
-plt.savefig("corner_fig/corner_kinematics.pdf", format="pdf")
-plt.close()
+    f = h5py.File("MElectrons_post.hdf5", "r")
+    df = pd.DataFrame(data=f.get("data"), columns=gen_columns + reco_columns)
+    f.close()
 
-# Corner plot: isolation
+    # Corner plot: kinematics
 
-labels = [
-    "MElectron_pt",
-    "MElectron_eta",
-    "MElectron_jetRelIso",
-    "MElectron_miniPFRelIso_all",
-    "MElectron_miniPFRelIso_chg",
-    "MElectron_mvaFall17V1Iso",
-    "MElectron_mvaFall17V1noIso",
-    "MElectron_mvaFall17V2Iso",
-    "MElectron_mvaFall17V2noIso",
-    "MElectron_pfRelIso03_all",
-    "MElectron_pfRelIso03_chg",
-]
+    df["MElectron_pt"] = df["MElectron_ptRatio"] * df["MGenElectron_pt"]
+    df["MElectron_eta"] = df["MElectron_etaMinusGen"] + df["MGenElectron_eta"]
+    df["MElectron_phi"] = df["MElectron_phiMinusGen"] + df["MGenElectron_phi"]
 
-fig = corner(
-    df[labels],
-    labels=labels,
-    color="tab:blue",
-    levels=(0.5, 0.9, 0.99),
-    hist_bin_factor=3,
-    scale_hist=True,
-    plot_datapoints=False,
-)
-plt.suptitle("MElectrons isolation variables", fontsize=20)
-plt.savefig("corner_fig/corner_isolation.pdf", format="pdf")
-plt.close()
+    labels = ["MElectron_pt", "MElectron_eta", "MElectron_phi"]
 
-# Corner plot: ip
+    fig = corner.corner(
+        df[labels],
+        labels=labels,
+        color="tab:blue",
+        levels=(0.5, 0.9, 0.99),
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+    )
+    plt.suptitle("MElectrons kinematics variables", fontsize=20)
+    plt.savefig("corner_fig/corner_kinematics.pdf", format="pdf")
+    plt.close()
 
-labels = [
-    "MElectron_pt",
-    "MElectron_eta",
-    "MElectron_ip3d",
-    "MElectron_sip3d",
-    "MElectron_dxy",
-    "MElectron_dxyErr",
-    "MElectron_dz",
-    "MElectron_dzErr",
-]
+    # Corner plot: isolation
 
-fig = corner(
-    df[labels],
-    labels=labels,
-    color="tab:blue",
-    levels=(0.5, 0.9, 0.99),
-    hist_bin_factor=3,
-    scale_hist=True,
-    plot_datapoints=False,
-)
-plt.suptitle("MElectrons ip variables", fontsize=20)
-plt.savefig("corner_fig/corner_ip.pdf", format="pdf")
-plt.close()
+    labels = [
+        "MElectron_pt",
+        "MElectron_eta",
+        "MElectron_jetRelIso",
+        "MElectron_miniPFRelIso_all",
+        "MElectron_miniPFRelIso_chg",
+        "MElectron_mvaFall17V1Iso",
+        "MElectron_mvaFall17V1noIso",
+        "MElectron_mvaFall17V2Iso",
+        "MElectron_mvaFall17V2noIso",
+        "MElectron_pfRelIso03_all",
+        "MElectron_pfRelIso03_chg",
+    ]
 
-# Corner plot: supercluster
+    fig = corner.corner(
+        df[labels],
+        labels=labels,
+        color="tab:blue",
+        levels=(0.5, 0.9, 0.99),
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+    )
+    plt.suptitle("MElectrons isolation variables", fontsize=20)
+    plt.savefig("corner_fig/corner_isolation.pdf", format="pdf")
+    plt.close()
 
-labels = [
-    "MElectron_pt",
-    "MElectron_eta",
-    "MElectron_sieie",
-    "MElectron_r9",
-    "MElectron_mvaFall17V1Iso",
-    "MElectron_mvaFall17V1noIso",
-    "MElectron_mvaFall17V2Iso",
-    "MElectron_mvaFall17V2noIso",
-]
+    # Corner plot: ip
 
-fig = corner(
-    df[labels],
-    labels=labels,
-    color="tab:blue",
-    levels=(0.5, 0.9, 0.99),
-    hist_bin_factor=3,
-    scale_hist=True,
-    plot_datapoints=False,
-)
-plt.suptitle("MElectrons supercluster variables", fontsize=20)
-plt.savefig("corner_fig/corner_supercluster.pdf", format="pdf")
-plt.close()
+    labels = [
+        "MElectron_pt",
+        "MElectron_eta",
+        "MElectron_ip3d",
+        "MElectron_sip3d",
+        "MElectron_dxy",
+        "MElectron_dxyErr",
+        "MElectron_dz",
+        "MElectron_dzErr",
+    ]
+
+    fig = corner.corner(
+        df[labels],
+        labels=labels,
+        color="tab:blue",
+        levels=(0.5, 0.9, 0.99),
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+    )
+    plt.suptitle("MElectrons ip variables", fontsize=20)
+    plt.savefig("corner_fig/corner_ip.pdf", format="pdf")
+    plt.close()
+
+    # Corner plot: supercluster
+
+    labels = [
+        "MElectron_pt",
+        "MElectron_eta",
+        "MElectron_sieie",
+        "MElectron_r9",
+        "MElectron_mvaFall17V1Iso",
+        "MElectron_mvaFall17V1noIso",
+        "MElectron_mvaFall17V2Iso",
+        "MElectron_mvaFall17V2noIso",
+    ]
+
+    fig = corner.corner(
+        df[labels],
+        labels=labels,
+        color="tab:blue",
+        levels=(0.5, 0.9, 0.99),
+        hist_bin_factor=3,
+        scale_hist=True,
+        plot_datapoints=False,
+    )
+    plt.suptitle("MElectrons supercluster variables", fontsize=20)
+    plt.savefig("corner_fig/corner_supercluster.pdf", format="pdf")
+    plt.close()
--- training/electrons/validate_electrons.py
+++ training/electrons/validate_electrons.py
@@ -11,6 +11,9 @@ import corner
 
 from scipy.stats import wasserstein_distance
 
+from postprocessing import postprocessing, gen_columns, reco_columns
+from corner_plots import make_corner
+
 
 def validate_electrons(
     test_loader,
@@ -32,7 +35,8 @@ def validate_electrons(
     else:
         save_dir = None
 
-    
+    # Generate samples
+
     with torch.no_grad():
 
         gen = []
@@ -45,8 +49,8 @@ def validate_electrons(
             inputs_y = y.to(device)
 
             z_sampled = model.sample(
-                    num_samples=1, context=inputs_y.view(-1, args.y_dim)
-                )
+                num_samples=1, context=inputs_y.view(-1, args.y_dim)
+            )
             z_sampled = z_sampled.cpu().detach().numpy()
             inputs_y = inputs_y.cpu().detach().numpy()
             z = z.cpu().detach().numpy()
@@ -55,6 +59,118 @@ def validate_electrons(
             reco.append(z)
             samples.append(z_sampled)
 
+    # Making DataFrames
+
     gen = np.array(gen).reshape((-1, args.y_dim))
     reco = np.array(reco).reshape((-1, args.zdim))
     samples = np.array(samples).reshape((-1, args.zdim))
+
+    gen = pd.DataFrame(data=gen, columns=gen_columns)
+    reco = pd.DataFrame(data=reco, columns=reco_columns)
+    samples = pd.DataFrame(data=samples, columns=reco_columns)
+
+    # Postprocessing for both test and samples datasets
+
+    gen = postprocessing(gen)
+    reco = postprocessing(reco)
+    samples = postprocessing(samples)
+
+    # Return to physical kinematic variables
+
+    for df in [reco, samples]:
+        df["MElectron_pt"] = df["MElectron_ptRatio"] * gen["MGenElectron_pt"]
+        df["MElectron_eta"] = df["MElectron_etaMinusGen"] + gen["MGenElectron_eta"]
+        df["MElectron_phi"] = df["MElectron_phiMinusGen"] + gen["MGenElectron_phi"]
+
+    # 1D FlashSim/FullSim comparison
+
+    for column in reco_columns:
+        ws = wasserstein_distance(reco[column], samples[column])
+
+        fig, axs = plt.subplots(1, 2, figsize=(9, 4.5), tight_layout=False)
+        fig.suptitle(f"{column} comparison")
+
+        # RECO histogram
+        _, rangeR, _ = axs[0].hist(
+            reco[column], histtype="step", lw=1, bins=100, label="FullSim"
+        )
+
+        # Saturation based on FullSim range
+        x = np.where(samples[column] < np.min(rangeR), np.min(rangeR), samples[column])
+        x = np.where(samples[column] > np.max(rangeR), np.max(rangeR), samples[column])
+
+        # Samples histogram
+        axs[0].hist(
+            x,
+            histtype="step",
+            lw=1,
+            range=[np.min(rangeR), np.max(rangeR)],
+            bins=100,
+            label=f"FlashSim, ws={round(ws, 4)}",
+            )
+        
+        axs[0].legend(frameon=False, loc="upper right")
+
+        # Log-scale comparison
+
+        axs[1].set_yscale("log")
+        axs[1].hist(reco[column], histtype="step", lw=1, bins=100)
+        axs[1].hist(x, histtype="step", lw=1, range=[np.min(rangeR), np.max(rangeR)], bins=100)
+        writer.add_figure(f"{column}", fig, global_step=epoch)
+        plt.close()
+        del x
+
+    # Corner plots
+
+    # Isolation
+
+    labels = [
+        "MElectron_pt",
+        "MElectron_eta",
+        "MElectron_jetRelIso",
+        "MElectron_miniPFRelIso_all",
+        "MElectron_miniPFRelIso_chg",
+        "MElectron_mvaFall17V1Iso",
+        "MElectron_mvaFall17V1noIso",
+        "MElectron_mvaFall17V2Iso",
+        "MElectron_mvaFall17V2noIso",
+        "MElectron_pfRelIso03_all",
+        "MElectron_pfRelIso03_chg",
+    ]
+
+    fig = make_corner(reco, samples, labels, "Isolation")
+    writer.add_figure("Isolation", fig, global_step=epoch)
+
+    # Impact parameter (range)
+
+    labels = [
+        "MElectron_pt",
+        "MElectron_eta",
+        "MElectron_ip3d",
+        "MElectron_sip3d",
+        "MElectron_dxy",
+        "MElectron_dxyErr",
+        "MElectron_dz",
+        "MElectron_dzErr",
+    ]
+
+    fig = make_corner(reco, samples, labels, "Impact parameter")
+    writer.add_figure("Impact parameter", fig, global_step=epoch)
+
+    # Impact parameter comparison (range)
+
+    reco["MElectron_sqrt_xy_z"] = np.sqrt((reco["MElectron_dxy"].values)**2 + (reco["MElectron_dz"].values)**2)
+    samples["MElectron_sqrt_xy_z"] = np.sqrt((samples["MElectron_dxy"].values)**2 + (samples["MElectron_dz"].values)**2)
+
+    labels = ["MElectron_sqrt_xy_z", "MElectron_ip3d"]
+
+    fig = make_corner(reco, samples, labels, r"Impact parameter vs \sqrt(dxy^2 + dz^2)")
+    writer.add_figure(r"Impact parameter vs \sqrt(dxy^2 + dz^2)", fig, global_step=epoch)
+
+    # Kinematics
+
+    labels = ["MElectron_pt", "MElectron_eta", "MElectron_phi"]
+
+    fig = make_corner(reco, samples, labels, "Kinematics")
+    writer.add_figure("Kinematics", fig, global_step=epoch)
+

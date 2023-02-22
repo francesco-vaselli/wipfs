1d0f78bcfe3dabb2510601fe8753f9ffdc1e2144
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-22 15:05:05 +0100

    ElectronDataset start:limit fixed

--- utils/dataset.py
+++ utils/dataset.py
@@ -419,8 +419,8 @@ class ElectronDataset(Dataset):
         self._archives = [h5py.File(h5_path, "r") for h5_path in self.h5_paths]
         self._archives = None
 
-        y = self.archives[0]["data"][start:limit, 0:y_dim]
-        x = self.archives[0]["data"][start:limit, y_dim : (y_dim + x_dim)]
+        y = self.archives[0]["data"][start:(start + limit), 0:y_dim]
+        x = self.archives[0]["data"][start:(start + limit), y_dim : (y_dim + x_dim)]
         self.x_train = torch.tensor(x, dtype=torch.float32)  # .to(device)
         self.y_train = torch.tensor(y, dtype=torch.float32)  # .to(device)
 

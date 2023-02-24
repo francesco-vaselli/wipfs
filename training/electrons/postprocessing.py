39c8d0fbff7720b3d680bce63ac1e505e16f036e
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-24 15:43:14 +0100

    Postprocessing bug resolved

--- training/electrons/postprocessing.py
+++ training/electrons/postprocessing.py
@@ -19,10 +19,6 @@ def restore_range(column_name, scale_dict, df):
 
 def inverse_transform(df, column_name, function, p):
 
-    if column_name == "MElectron_dr03EcalRecHitSumEt":
-        print("SONO DENTRO")
-
-    print(f"Applying {function} with parameters {p}...")
     df[column_name] = df[column_name].apply(lambda x: (function(x) - p[1]) / p[0])
     return df[column_name]
 
@@ -33,8 +29,6 @@ def unsmearing(df, column_name, interval):
     in the selected interval, and then we just have to compute the sample mean
     in this range.
     """
-    print("Unsmearing")
-
     val = df[column_name].values
     if interval != None:
         mask_condition = np.logical_and(val >= interval[0], val <= interval[1])
@@ -56,8 +50,6 @@ def cut_unsmearing(df, column_name, cut, x1, x2):
 
 
 def process_column_var(column_name, operations, df):
-
-    print(f"Processing {column_name}...")
 
     print(operations)
     for op in operations:

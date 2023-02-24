840607b5530f528b342568b3cf96654816ccaf80
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-24 14:35:24 +0100

    Postprocessing debugging

--- training/electrons/postprocessing.py
+++ training/electrons/postprocessing.py
@@ -19,6 +19,7 @@ def restore_range(column_name, scale_dict, df):
 
 def inverse_transform(df, column_name, function, p):
 
+    print(f"Transformation of {column_name}")
     df[column_name] = df[column_name].apply(lambda x: (function(x) - p[1]) / p[0])
     return df[column_name]
 

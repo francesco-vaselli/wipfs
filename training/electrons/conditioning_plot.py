9d6d6deee70e076fa46ae2bd670ff399d83836be
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-27 15:43:51 +0100

    Conditioning_plot function for validation

---
+++ training/electrons/conditioning_plot.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def conditioning_plot(reco, samples, gen, target_col, condition_col, *args, **kwargs):
+
+    full = reco[target_col].values
+    flash = samples[target_col].values
+    conditioning = gen[condition_col].values
+
+    # Mask
+
+    full = full[conditioning]
+    flash = flash[conditioning]
+
+    fig = plt.figure()
+    plt.hist(full, histtype="step", label="FullSim", ls="--", *args, **kwargs)
+    plt.hist(flash, histtype="step", label="FlashSim", *args, **kwargs)
+    plt.legend()
+    plt.title(f"{target_col}/{condition_col}")
+    plt.show()
+    plt.close()
+
+    return fig
+
+
+if __name__ == "__main__":
+
+    np.random.seed(0)
+
+    N = 10000
+    x = np.random.rand(N)
+    x = pd.DataFrame(data=x, columns=["a"])
+
+    y = np.random.rand(N)
+    y = pd.DataFrame(data=y, columns=["a"])
+
+    g = np.random.randint(0, 2, size=N)
+    g = pd.DataFrame(data=g, columns=["b"])
+
+
+    fig = conditioning_plot(x, y, g, "a", "b", bins=10, range=[0, 0.5])
+    fig.show()
+    

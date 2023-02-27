df4970e1aafb19f11c95bd79f337c67aa122701a
fcattafesta <filippo.cattafesta99@gmail.com>
2023-02-27 15:45:04 +0100

    Conditioning_plot: plt.show() removed

--- training/electrons/conditioning_plot.py
+++ training/electrons/conditioning_plot.py
@@ -19,7 +19,6 @@ def conditioning_plot(reco, samples, gen, target_col, condition_col, *args, **kw
     plt.hist(flash, histtype="step", label="FlashSim", *args, **kwargs)
     plt.legend()
     plt.title(f"{target_col}/{condition_col}")
-    plt.show()
     plt.close()
 
     return fig

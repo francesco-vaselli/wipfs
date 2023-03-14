6092f1f3208c7ddf50b2c3f706b0b0edd9f5991e
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-14 20:36:24 +0100

    correct context dim1

--- models/modded_MADE_mask.py
+++ models/modded_MADE_mask.py
@@ -244,7 +244,8 @@ class NMaskedMADE(nn.Module):
         )
 
         if context_features + features - NMask_degree > 0:
-            self.context_layer = nn.Linear(context_features - NMask_degree, hidden_features)
+            self.context_layer = nn.Linear(context_features + features - NMask_degree, hidden_features)
+            print("context_layer", self.context_layer)
 
         self.use_residual_blocks = use_residual_blocks
         self.activation = activation

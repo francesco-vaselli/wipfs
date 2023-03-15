921ebb70829f2f4a67ccd18e69e838d8be08f7e2
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-15 17:50:14 +0100

    prova.c

--- generation/electrons/prova.C
+++ generation/electrons/prova.C
@@ -1,6 +1,8 @@
 
 
-void prova(col) {
+void prova() {
+
+  auto col = "Electron_pt";
 
   ROOT::EnableImplicitMT();
 
@@ -13,8 +15,9 @@ void prova(col) {
 
   auto full_dy = ROOT::RDataFrame("Events", f);
 
-  auto g =
-      TFile::Open("~/16ADF854-8C85-DB4F-84F0-339B292E3CBD_synth.root", "r");
+  auto g = TFile::Open("/gpfs/ddn/cms/user/cattafe/DYJets/230000/"
+                       "8244ED99-0F95-9D4F-B393-22EBC589A46D_synth.root",
+                       "r");
 
   auto synth_dy = ROOT::RDataFrame("Events", g);
 
@@ -25,5 +28,28 @@ void prova(col) {
   h1->Draw();
   h2->Draw("same");
 
-  c->SaveAs(col + ".pdf");
+  c->SaveAs("dy.pdf");
+
+  auto m =
+      TFile::Open("~/16ADF854-8C85-DB4F-84F0-339B292E3CBD_synth.root", "r");
+
+  synt_tt = ROOT::RDataFrame("Events", m);
+
+  auto p = TFile::Open("/gpfs/ddn/srm/cms//store/mc/RunIIAutumn18NanoAODv6/"
+                       "TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/"
+                       "Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/"
+                       "60000/16ADF854-8C85-DB4F-84F0-339B292E3CBD.root",
+                       "r");
+
+  auto full_tt = ROOT::RDataFrame("Events", p);
+
+  auto h3 = full_tt.Histo1D(col);
+  auto h4 = synt_tt.Histo1D(col);
+
+  c = new TCanvas("c", "c", 800, 600);
+
+  h3->Draw();
+  h4->Draw("same");
+
+  c->SaveAs("tt.pdf");
 }

57990b5ff3f7f548dd6406b8d86b2987f9710ea6
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-16 14:53:32 +0100

    better dphi val2

--- training/fakes/validate_fakes.py
+++ training/fakes/validate_fakes.py
@@ -15,17 +15,13 @@ import pandas as pd
 
 
 def delta_phi1v9(pts, phis):
-    filtered_phi = np.where(pts > 0, phis, np.inf)
+    filtered_phi = np.where(pts > 0, phis, np.nan)
     dphi = np.expand_dims(filtered_phi[:, 0], axis=-1) - filtered_phi[:, 1:10]
-    dphi = dphi.flatten()
-    #dphi.reshape(-1, 9)
-    finite_mask = np.isfinite(dphi)
-    dphiF = dphi[finite_mask]
-    dhpiF = np.where(dphiF > np.pi, dphiF - 2 * np.pi, dphiF)
-    dhpiF = np.where(dphiF < -np.pi, dphiF + 2 * np.pi, dphiF)
-    dphi[finite_mask] = dhpiF
-    dphi[~finite_mask] = -7
     dphi = dphi.reshape(-1, 9)
+    # constraints the angles in the -pi,pi range
+    dphi = np.where(dphi > np.pi, dphi - 2 * np.pi, dphi)
+    dphi = np.where(dphi < -np.pi, dphi + 2 * np.pi, dphi)
+    dphi = np.where(dphi == np.nan, -5, dphi)
 
     return dphi
 

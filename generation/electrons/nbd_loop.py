9aed1d2134d461e3b3f002f0f827a414381bfa4e
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-16 14:18:31 +0100

    path updated

--- generation/electrons/nbd_loop.py
+++ generation/electrons/nbd_loop.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     # specify old/new root
     root = "/gpfs/ddn/srm/cms//store/mc/RunIIAutumn18NanoAODv6/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20_ext1-v1/"
-    new_root = "/gpfs/ddn/cms/user/cattafe/TTJets/"
+    new_root = "/gpfs/ddn/cms/user/cattafe/TTJets/EM1/"
     ttbar_training_files = [
         "250000/047F4368-97D4-1A4E-B896-23C6C72DD2BE.root",
         "240000/B38E7351-C9E4-5642-90A2-F075E2411B00.root",

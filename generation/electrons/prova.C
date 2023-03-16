323a87f0f0a58d6e07beaa0f47640c33e11e5ff7
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-16 13:49:34 +0100

    debug #7

--- generation/electrons/prova.C
+++ generation/electrons/prova.C
@@ -832,9 +832,10 @@ void prova() {
 
   auto c1 = new TCanvas("c1", "c1", 800, 600);
 
-  h_t->Draw("PLC HIST");
+  h3->Draw("PLC HIST");
+  h3->SetLineColor(kRed);
   h4->Draw("same PLC HIST");
-  h3->Draw("PLC HIST same");
+  h4->SetLineColor(kBlue);
 
   c1->SaveAs("tt.pdf");
 }

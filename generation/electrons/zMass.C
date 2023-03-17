4f70cb0cb1130bed3bf3112c723bae049f249fc9
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-17 16:31:59 +0100

    zMass

--- generation/electrons/zMass.C
+++ generation/electrons/zMass.C
@@ -776,6 +776,18 @@ auto InvariantMass(ROOT::VecOps::RVec<double> &pt,
   return (p1 + p2).mass();
 }
 
+auto InvariantMass_float(ROOT::VecOps::RVec<float> &pt,
+                   ROOT::VecOps::RVec<float> &eta,
+                   ROOT::VecOps::RVec<float> &phi) {
+
+  auto m = 0.51099895000e-3; // GeV
+
+  ROOT::Math::PtEtaPhiMVector p1(pt[0], eta[0], phi[0], m);
+  ROOT::Math::PtEtaPhiMVector p2(pt[1], eta[1], phi[1], m);
+
+  return (p1 + p2).mass();
+}
+
 void zMass() {
 
   ROOT::EnableImplicitMT();

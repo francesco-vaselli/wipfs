a54e56e9fdfa503ec9ae81251851087d14536b28
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-17 11:16:16 +0100

    pt in log

--- preproce/fake_jets/new_fake_jets.py
+++ preproce/fake_jets/new_fake_jets.py
@@ -109,8 +109,10 @@ def single_file_preprocess(filename: str) -> pd.DataFrame:
     df["angle"] = angle
 
     # saturate all pts to 200 GeV, then rescale by 200
-    df.iloc[:, :10] = df.iloc[:, :10].clip(upper=200)
-    df.iloc[:, :10] = df.iloc[:, :10] / 200
+    # df.iloc[:, :10] = df.iloc[:, :10].clip(upper=200)
+    # df.iloc[:, :10] = df.iloc[:, :10] / 200
+    # apply log to all pts
+    df.iloc[:, :10] = df.iloc[:, :10].apply(lambda x: np.log(x))
     # reorder columns to have pt, eta, phi for each jet
     idxs = np.vstack(
         (np.arange(0, 10), np.arange(10, 20), np.arange(20, 30))

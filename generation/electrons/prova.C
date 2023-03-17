65e6a097ca451a3b0022240372fa4de0ef24d1ef
fcattafesta <filippo.cattafesta99@gmail.com>
2023-03-17 15:10:54 +0100

    type

--- generation/electrons/prova.C
+++ generation/electrons/prova.C
@@ -797,11 +797,12 @@ void prova() {
 
   auto reco_pt = synth_dy.Take<ROOT::VecOps::RVec<float>>("Electron_pt");
 
+  cout << gen_pt << endl;
   auto h_pt = new TH2F("h_pt", "", 50, 0, 100, 50, 0, 100);
 
-  for (int i = 0; i < gen_pt->size(); i++) {
-    h_pt->Fill(gen_pt->at(i), reco_pt->at(i));
-  }
+  // for (int i = 0; i < gen_pt->size(); i++) {
+  //   h_pt->Fill(gen_pt->at(i), reco_pt->at(i));
+  // }
 
   auto c_pt = new TCanvas("c_pt", "c_pt", 800, 600);
 

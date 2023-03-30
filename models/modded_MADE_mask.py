a5a0a37f27a93ff4335d07b99e36e8dbbd06a567
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-30 17:23:42 +0200

    added 0 variable in args6

--- models/modded_MADE_mask.py
+++ models/modded_MADE_mask.py
@@ -73,6 +73,7 @@ class NMaskedLinear(nn.Linear):
         return mask, out_degrees
 
     def forward(self, x, context):
+        print(x.size(), torch.hstack((torch.zeros(x.shape[0], 1).to(x.device), context[:, -self.NMask_degree:])).size())
         return F.linear(x * torch.hstack((torch.zeros(x.shape[0], 1).to(x.device), context[:, -self.NMask_degree:])), self.weight * (self.mask), self.bias) # context mask should be reshaped?
 
 
@@ -233,7 +234,7 @@ class NMaskedMADE(nn.Module):
             raise ValueError("Residual blocks can't be used with random masks.")
         super().__init__()
 
-        NMask_degree = features
+        NMask_degree = features-1
         # Initial layer.
         self.initial_layer = NMaskedLinear(
             in_degrees=_get_input_degrees(features),
@@ -245,7 +246,7 @@ class NMaskedMADE(nn.Module):
         )
 
         if context_features + features - NMask_degree > 0:
-            self.context_layer = nn.Linear(context_features + features - NMask_degree, hidden_features)
+            self.context_layer = nn.Linear(context_features + features -1 - NMask_degree, hidden_features)
             print("context_layer", self.context_layer)
 
         self.use_residual_blocks = use_residual_blocks
@@ -291,7 +292,7 @@ class NMaskedMADE(nn.Module):
             temps = block(temps, context=context[:, :self.context_layer.in_features])
         outputs = self.final_layer(temps)
         outputs = outputs.view(inputs.shape[0], inputs.shape[1], -1)
-        mask = context[:, -inputs.shape[1]:]
+        mask = torch.hstack((torch.zeros(inputs.shape[0], 1).to(context.device), context[:, -inputs.shape[1]:]))
         outputs[mask==0, :] = np.log(np.exp(1 - 1e-3) - 1)
         outputs = outputs.view(inputs.shape[0], -1)
         return outputs

424d0d391be757bab3ab9e110e372de3ec85231a
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-30 17:39:57 +0200

    added 0 variable13

--- training/fakes/validate_fakes.py
+++ training/fakes/validate_fakes.py
@@ -97,11 +97,10 @@ def validate_fakes(
             inputs_y = inputs_y.cpu().detach().numpy()
             x = x.cpu().detach().numpy()
             zero_full = x[:, 0]
-            print(zero_full)
             x = x[:, 1:]
-            print(x_sampled.shape)
-            x_sampled = x_sampled.reshape(-1, args.x_dim)[:, 1:]
-            zero_flash = x_sampled.reshape(-1, args.x_dim)[:, 0]
+            x_sampled = x_sampled.reshape(-1, args.x_dim)
+            x_sampled = x_sampled[:, 1:]
+            zero_flash = x_sampled[:, 0]
             gen.append(inputs_y[:, :args.y_dim])
             reco.append(x)
             samples.append(x_sampled)

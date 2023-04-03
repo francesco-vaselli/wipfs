a1764e8c43df77a512c687fbff161e8d1743c952
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-04-03 11:08:56 +0200

    only rank

--- training/fakes/fakes_train.py
+++ training/fakes/fakes_train.py
@@ -40,11 +40,12 @@ def trainer(gpu, save_dir, ngpus_per_node, args, val_func):
     if args.distributed:
         if args.dist_url == "env://" and args.rank == -1:
             args.rank = int(os.environ["RANK"])
+            print("rank: {}".format(args.rank))
             # args.dist_url = os.environ["MASTER_ADDR"]
             args.gpu = int(os.environ["LOCAL_RANK"])
             print("Now Use GPU: {} for training".format(args.gpu))
-        if args.distributed:
-            args.rank = args.rank * ngpus_per_node + gpu
+        # if args.distributed:
+        #     args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(
             backend=args.dist_backend,
             init_method= args.dist_url, # dist.TCPStore("r246n05", 29800),# args.dist_url,

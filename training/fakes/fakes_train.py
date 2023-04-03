7c5667015ca628b3ad22096f6a9ba42dcca1928a
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-04-03 10:53:22 +0200

    local rank for gpu

--- training/fakes/fakes_train.py
+++ training/fakes/fakes_train.py
@@ -41,6 +41,7 @@ def trainer(gpu, save_dir, ngpus_per_node, args, val_func):
         if args.dist_url == "env://" and args.rank == -1:
             args.rank = int(os.environ["RANK"])
             # args.dist_url = os.environ["MASTER_ADDR"]
+            args.gpu = int(os.environ["LOCAL_RANK"])
         if args.distributed:
             args.rank = args.rank * ngpus_per_node + gpu
         dist.init_process_group(
